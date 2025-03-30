f2df71c430d4ccd5f941cdd9b4683e9d6cccd55a
Sri-Aryan <aryansri.official@gmail.com>
2025-03-30 17:53:30 +0530

    Intial Commit

---
+++ app/src/main/java/com/example/newsapiapp/wrapper/Resource.kt
@@ -0,0 +1,14 @@
+package com.example.newsapiapp.wrapper
+
+sealed class Resource<T> (
+
+
+    val data: T? = null,
+    val message: String? = null)
+
+    {
+        class Success<T>(data: T) : Resource<T>(data)
+        class Error<T>(message: String, data: T?= null) : Resource<T>(data, message)// if there is an error and body to it
+        class Loading<T>: Resource<T>()
+
+    }
